2a5d863520660298e92e5ff1d52bed2173dc8f1f
esegev1 <esegev1@gmail.com>
2025-11-21 12:01:35 -0500

    lab submission v2

--- find_carmen.sql
+++ find_carmen.sql
@@ -70,10 +70,10 @@ AND a.name LIKE '%Ser%';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 SELECT
-    a.name,
+    a.name AS current_city,
+    b.name AS country,
     b.region,
-    b.capital,
-    c.name
+    c.name AS capital_city
 FROM city AS a
 LEFT JOIN country AS b
     ON a.countrycode = b.code
